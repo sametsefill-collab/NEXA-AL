7eb1697367b663a620d3faa68cf01011bb51b093
sametsefill-collab <sametsefill@gmail.com>
2026-08-27 17:08:01 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const NEXA_CONFIG = {
+  appName: "NEXA-AL",
+  version: "1.0.0",
+  environment: "development",
+
+  api: {
+    baseUrl: "",
+    websocketUrl: ""
+  },
+
+  market: {
+    defaultSymbol: "BTCUSDT",
+    currency: "USDT"
+  }
+};
