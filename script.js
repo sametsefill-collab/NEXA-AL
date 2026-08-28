46e03d24caa3bb74a062e3bcb4275bd62dc5ec5c
sametsefill-collab <sametsefill@gmail.com>
2026-08-28 20:54:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ if (aiBtn && panel) {
 
  async function getAnswer(text) {
   try {
-    const response = await fetch("https://nexa-al.vercel.app/api/chat" {
+    const response = await fetch("https://nexa-al.vercel.app/api/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
